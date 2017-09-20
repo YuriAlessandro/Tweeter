84fac3c5554e8e66c998e99379804fe6197af228
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2017-09-20 16:33:14 -0300

    Get device ip

--- app/src/main/java/com/example/tweeter/ChatActivity.java
+++ app/src/main/java/com/example/tweeter/ChatActivity.java
@@ -1,13 +1,29 @@
 package com.example.tweeter;
 
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
+    private TextView textIp;
+    private Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        textIp = (TextView) findViewById(R.id.deviceIp);
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext()
+                .getSystemService(WIFI_SERVICE);
+        String ipAddress = String.valueOf(wifiManager.getConnectionInfo().getIpAddress());
+        textIp.setText(ipAddress);
+
+        btnSend = (Button) findViewById(R.id.sendDirect);
     }
 }
